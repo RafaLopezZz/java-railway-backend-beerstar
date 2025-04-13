0a7eb75d4c763f2c5fa25b99eaffcc157b5d8a14
RafaLopezZz <142743097+RafaLopezZz@users.noreply.github.com>
2025-04-13 11:54:32 +0200

    testing CORS #12

--- src/main/java/com/tfc/beerstar/config/SecurityConfig.java
+++ src/main/java/com/tfc/beerstar/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.tfc.beerstar.config;
 
+import java.util.Arrays;
+
 //import java.util.Arrays;
-//import java.util.List;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +15,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 //import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 //import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 //import org.springframework.web.servlet.config.annotation.CorsRegistry;
 //import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -36,7 +39,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         log.info("Configurando seguridad HTTP");     
         http.csrf(csrf -> csrf.disable())
-            .cors(null)
+            .cors(cors -> cors.configurationSource(this.corsConfigurationSource()))
             //.cors(cors -> cors.configurationSource(this.corsConfigurationSource()))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/beerstar/**").permitAll()
@@ -50,6 +53,19 @@ public class SecurityConfig {
 
     
     // Configuración de CORS
+        @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     /* 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
