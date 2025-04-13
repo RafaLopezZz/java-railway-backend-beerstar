493cdd1dc8e35fe47b842477f2466ba1f1ab0744
RafaLopezZz <142743097+RafaLopezZz@users.noreply.github.com>
2025-04-13 22:24:15 +0200

    testing CORS #21

--- src/main/java/com/tfc/beerstar/config/SecurityConfig.java
+++ src/main/java/com/tfc/beerstar/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.tfc.beerstar.config;
 
 
-import java.util.Arrays;
-import java.util.List;
+//import java.util.Arrays;
+//import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +13,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -34,11 +35,12 @@ public class SecurityConfig {
                 //.requestMatchers("/beerstar/proveedores/**").authenticated() // Para autenticar usuarios con login
             )
             .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+            .cors(withDefaults());
             log.info("Configuración de seguridad HTTP completada");
         return http.build();
     }
 
+    /* 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -51,6 +53,7 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+    */
 
     @Bean
     public PasswordEncoder passwordEncoder() {
