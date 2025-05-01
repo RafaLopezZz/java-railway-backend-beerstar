783c79dc4244d34d5fd616f29ac33b4379d700d2
RafaLopezZz <142743097+RafaLopezZz@users.noreply.github.com>
2025-05-01 19:53:56 +0200

    Version Articulos, Categorias y Lotes

--- src/main/java/com/tfc/beerstar/exception/ResourceNotFoundException.java
+++ src/main/java/com/tfc/beerstar/exception/ResourceNotFoundException.java
@@ -1,22 +1,10 @@
 package com.tfc.beerstar.exception;
 
 /**
- * Excepción personalizada que se lanza cuando un recurso solicitado
- * no existe o no puede ser encontrado en el sistema.
- *
- * <p>Esta excepción suele utilizarse en servicios donde se buscan entidades
- * por ID u otros atributos únicos, y no se encuentra una coincidencia.</p>
- *
- * @author rafalopezzz
+ * Excepción para cuando no se encuentra un recurso (por ejemplo, un usuario).
+ * Se utiliza para devolver un error 404.
  */
-
 public class ResourceNotFoundException extends RuntimeException {
-    /**
-     * Crea una nueva instancia de {@code ResourceNotFoundException}
-     * con el mensaje proporcionado.
-     *
-     * @param message Mensaje descriptivo sobre el recurso no encontrado.
-     */
     public ResourceNotFoundException(String message) {
         super(message);
     }
