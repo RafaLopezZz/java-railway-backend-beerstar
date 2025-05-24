df02c71eea4fc40be4cd6561f7482e5b13bb70be
RafaLopezZz <142743097+RafaLopezZz@users.noreply.github.com>
2025-05-24 20:52:03 +0200

    Swagger #5

--- src/main/java/com/tfc/beerstar/config/SecurityConfig.java
+++ src/main/java/com/tfc/beerstar/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth
-                        -> auth.requestMatchers("/rlp/auth/**").permitAll()
+                        -> auth.requestMatchers("/", "/rlp/auth/**").permitAll()
                         .requestMatchers(
                                 "/v3/api-docs",
                                 "/v3/api-docs/**",
